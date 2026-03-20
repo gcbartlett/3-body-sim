ab1abca8bbc7e7f8cad0e42acacbd125860782e9
Gary Bartlett <Developer@GaryBartlett.com>
2026-03-20 11:22:29 -0700

    Add ESLint and TypeScript ESLint integrations
    Configured the project with ESLint and TypeScript ESLint for code linting. Added linting and autofix scripts to `package.json` and updated `devDependencies`.

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default tseslint.config(
+    {
+        ignores: ["dist/**", "node_modules/**"],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended
+);
